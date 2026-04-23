7594956e9002da4bf5caebf3fab068d0edf4d191
semzv <85784802+semzev@users.noreply.github.com>
2026-04-23 18:41:20 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,31 @@
+<!DOCTYPE html> 
 
+<html>
+  <head>
+    <title>Roblox command panel</title>
+  </head>
+
+  <body>
+    <form method="post" action="">
+      <label>Speler naam:</label>
+      <input name="player" type="text">
+      <button type="submit">Kick</button>
+
+    </form>
+  </body>
+
+</html>
+
+
+<?php
+
+$filename = "commands.json";
+
+if (isset($_POST["player"])) {
+  $player = $_POST["player"];
+  file_put_contents($filename, json_encode(["player" => $player], JSON_PRETTY_PRINT));
+
+  echo file_get_contents($filename);
+}
+
+?>
